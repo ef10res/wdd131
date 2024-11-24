9684b378a5ebdf945557b83faac928f2344a3a4f
ef10res <floreserick10@gmail.com>
2024-11-24 16:24:23 -0700

    activities

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,22 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('list');
-const li = document.querySelector('li');
-const deleteButton = document.querySelector('button');
-li.textContent = input.value;
-deleteButton.textContent = 'X';
-li.append(deleteButton);
-list.append(li);
+
+addChapter.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const input = document.querySelector('#favchap');
+        const button = document.querySelector('button');
+        const list = document.querySelector('list');
+        const li = document.querySelector('li');
+        const deleteButton = document.querySelector('button');
+        li.textContent = input.value;
+        deleteButton.textContent = 'X';
+        li.append(deleteButton);
+        list.append(li);
+    }
+});
+
+deleteButton.addEventListener('clicl', function () {
+    list.removeChild(li);
+    input.focus();
+})
+
+input.value = '';
+input.focus();
