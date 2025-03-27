4ddc314206ff76a099a9ecde07fb47fb5b58b9cc
ef10res <floreserick10@gmail.com>
2025-03-26 22:16:11 -0600

    week02

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,23 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('list');
-const li = document.createElement('li');
-const delbutton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = 'X';
-li.append(deleteButton);
-list.append(li);
+
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const input = document.querySelector('#favchap');
+        const button = document.querySelector('button');
+        const list = document.querySelector('list');
+        const li = document.createElement('li');
+        const delbutton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = 'X';
+        li.append(deleteButton);
+        list.append(li);
+    }
+});
+
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus()
+})
+
+input.value = '';
+input.focus();
