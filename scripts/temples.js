fffcedb6aeb4c95717fc4c1379fc7b19b3d20cc3
ef10res <floreserick10@gmail.com>
2025-03-28 21:52:29 -0600

    fixed footer

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,12 @@ hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
 });
+
+const lastModifiedDate = new Date(document.lastModified);
+const year = lastModifiedDate.getFullYear();
+const month = String(lastModifiedDate.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+const day = String(lastModifiedDate.getDate()).padStart(2, '0');
+
+const formattedDate = `${year}-${month}-${day}`;
+
+document.getElementById('lastModified').textContent = formattedDate;
